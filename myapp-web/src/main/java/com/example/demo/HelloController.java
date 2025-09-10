bd58a65829b4c0714b5e335bdb99098c393226f6
zz <1730581251@qq.com>
2025-09-10 16:41:02 +0800

    分离hello与运行

--- myapp-web/src/main/java/com/example/demo/HelloController.java
+++ myapp-web/src/main/java/com/example/demo/HelloController.java
@@ -1,16 +1,10 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class MyAppWebApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MyAppWebApplication.class, args);
-    }
-}
+
 
 @RestController
 class HelloController {
